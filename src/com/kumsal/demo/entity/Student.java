1862b437e65c9b9eea847eb7f4fb48815de4b391
kumsal65 <yahya.alatas.mdbf17@iste.edu.tr>
2020-10-26 15:19:55 +0300

    generated fıeld

--- src/com/kumsal/demo/entity/Student.java
+++ src/com/kumsal/demo/entity/Student.java
@@ -21,6 +21,13 @@ public class Student {
 	
 	@Column(name="email")
 	private String email;
+
+	public Student(String firstName, String lastName, String email) {
+		super();
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+	}
 	
 	
 }
