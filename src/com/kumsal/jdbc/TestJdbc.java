47d66a1470537a86ef7c946eb8068d1af92eec09
kumsal65 <yahya.alatas.mdbf17@iste.edu.tr>
2020-10-26 14:47:47 +0300

    add new hibernate ile

--- src/com/kumsal/jdbc/TestJdbc.java
+++ src/com/kumsal/jdbc/TestJdbc.java
@@ -6,15 +6,16 @@ import java.sql.DriverManager;
 public class TestJdbc {
 
 	public static void main(String[] args) {
-		String jdbcConnection="jdbc:mysql://localhost:3306/hb_student_tracker?useSSL=false";
+		String jdbcConnection="jdbc:mysql://localhost:3306/hb_student_tracker?useSSL=false&serverTimezone=GMT";
 		String user="hbstudent";
 		String password="hbstudent";
 		
 		try {
+			System.out.println("Connecting");
 			Connection connection=DriverManager.getConnection(jdbcConnection,user,password);
 			System.out.println("Connection success");
 		} catch (Exception e) {
-			// TODO: handle exception
+			System.out.println(e);
 		}
 	}
 }
