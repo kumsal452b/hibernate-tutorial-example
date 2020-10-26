eb4ea6a1f43b15c54f1479bbb7835a301f829602
kumsal65 <yahya.alatas.mdbf17@iste.edu.tr>
2020-10-26 15:15:04 +0300

    add new students elemetn for databse

--- src/com/kumsal/demo/entity/Student.java
+++ src/com/kumsal/demo/entity/Student.java
@@ -1,5 +1,16 @@
 package com.kumsal.demo.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="student")
 public class Student {
 
+	private int id;
+	private String firstName;
+	private String lastName;
+	private String email;
+	
+	
 }
