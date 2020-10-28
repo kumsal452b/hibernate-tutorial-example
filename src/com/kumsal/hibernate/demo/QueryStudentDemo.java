2c3aa02d87b5b8cc0113b43698deff9085dcdee3
kumsal65 <yahya.alatas.mdbf17@iste.edu.tr>
2020-10-28 16:32:46 +0300

    add new sessıon and factory element

--- src/com/kumsal/hibernate/demo/QueryStudentDemo.java
+++ src/com/kumsal/hibernate/demo/QueryStudentDemo.java
@@ -1,7 +1,22 @@
 package com.kumsal.hibernate.demo;
 
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.kumsal.demo.entity.Student;
+
+
 public class QueryStudentDemo {
 	public static void main(String[] args) {
+		SessionFactory theSessions=new Configuration()
+				.configure("hibernate.cfg.xml")
+				.addAnnotatedClass(Student.class)
+				.buildSessionFactory();
+		Session session=theSessions.getCurrentSession();
 		
+		List<Student> theStudents=session.query
 	}
 }
