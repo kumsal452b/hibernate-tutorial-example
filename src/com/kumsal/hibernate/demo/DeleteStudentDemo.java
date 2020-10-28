622d8e8a0a85a1b340195dc069641858d7b5a772
kumsal65 <yahya.alatas.mdbf17@iste.edu.tr>
2020-10-28 18:11:27 +0300

    add new class name x

---
+++ src/com/kumsal/hibernate/demo/DeleteStudentDemo.java
@@ -0,0 +1,34 @@
+package com.kumsal.hibernate.demo;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.kumsal.demo.entity.Student;
+
+import sun.print.resources.serviceui_zh_TW;
+
+public class DeleteStudentDemo {
+
+	public static void main(String[] args) {
+		SessionFactory sessionFactory=new Configuration()
+				.configure("hibernate.cfg.xml")
+				.addAnnotatedClass(Student.class)
+				.buildSessionFactory();
+		
+		Session session=sessionFactory.getCurrentSession();
+		
+		try {
+			int id=1;
+			session.beginTransaction();
+			Student theStudent=session.get(Student.class, id);
+			theStudent.setLastName("Alatas");
+			theStudent.setEmail("kms@gmail.com");
+			session.getTransaction().commit();
+		} finally {
+			// TODO: handle finally clause
+		}
+
+	}
+
+}
